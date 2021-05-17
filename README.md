6fb4ec981e180c77ca7ff520b0d2a8f85f8cacc7
cronicc <cronic@zensystem.io>
2021-05-17 22:46:43 +0000

    Add sc-ci-base images and CI pipeline

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
-# sc-ci-base-docker
+![Travis (.com) branch](https://img.shields.io/travis/com/HorizenOfficial/sc-ci-base-docker/master) ![Docker Stars](https://img.shields.io/docker/stars/zencash/sc-ci-base.svg) ![Docker Pulls](https://img.shields.io/docker/pulls/zencash/sc-ci-base.svg)
+
+## Docker base images for Zendoo CI pipelines
+
+### About
+Provides Docker base images for CI pipelines of the following Zendoo repositories:
+- [ginger-lib](https://github.com/HorizenOfficial/ginger-lib)
+- [marlin](https://github.com/HorizenLabs/marlin)
+- [poly-commit](https://github.com/HorizenLabs/poly-commit)
+- [zendoo-cctp-lib](https://github.com/HorizenOfficial/zendoo-cctp-lib)
+- [zendoo-mc-cryptolib](https://github.com/HorizenOfficial/zendoo-mc-cryptolib)
+- [zendoo-sc-cryptolib](https://github.com/HorizenOfficial/zendoo-sc-cryptolib)
+
+Multiple images in various combinations are being built. They can be generalized into three types:
+1. Rust, default gcc and mingw toolchain for Ubuntu Linux with the Rust cross compile target for x86_64-pc-windows-gnu installed
+2. OpenJDK installed from https://adoptopenjdk.net/ and maven for Ubuntu Linux
+3. A combination of 1. and 2. with both Rust and OpenJDK
+
+- For the Rust images `stable` and `nightly` based images are built
+- For the OpenJDK images JDK versions `8`, `11` and `16` (at the time of writing the latest available JDK version) are built
+- For the images containing both Rust and OpenJDK combinations of all available versions are built
+
+For all of these images three versions from the following base images are being built: `ubuntu:bionic`, `ubuntu:focal` and `ubuntu:hirsute`
+
+The images are tagged using the followig schema:
+```
+zencash/sc-ci-base:{bionic,focal,hirsute}_rust-{stable,nightly}_{$(date +%Y%m%d),latest};
+zencash/sc-ci-base:{bionic,focal,hirsute}_jdk-{8,11,16}_{$(date +%Y%m%d),latest};
+zencash/sc-ci-base:{bionic,focal,hirsute}_rust-{stable,nightly}_jdk-{8,11,16}_{$(date +%Y%m%d),latest};
+```
+
+### Available tags (only latest shown)
+```
+zencash/sc-ci-base:bionic_jdk-11_latest
+zencash/sc-ci-base:bionic_jdk-16_latest
+zencash/sc-ci-base:bionic_jdk-8_latest
+zencash/sc-ci-base:bionic_rust-nightly_jdk-11_latest
+zencash/sc-ci-base:bionic_rust-nightly_jdk-16_latest
+zencash/sc-ci-base:bionic_rust-nightly_jdk-8_latest
+zencash/sc-ci-base:bionic_rust-nightly_latest
+zencash/sc-ci-base:bionic_rust-stable_jdk-11_latest
+zencash/sc-ci-base:bionic_rust-stable_jdk-16_latest
+zencash/sc-ci-base:bionic_rust-stable_jdk-8_latest
+zencash/sc-ci-base:bionic_rust-stable_latest
+zencash/sc-ci-base:focal_jdk-11_latest
+zencash/sc-ci-base:focal_jdk-16_latest
+zencash/sc-ci-base:focal_jdk-8_latest
+zencash/sc-ci-base:focal_rust-nightly_jdk-11_latest
+zencash/sc-ci-base:focal_rust-nightly_jdk-16_latest
+zencash/sc-ci-base:focal_rust-nightly_jdk-8_latest
+zencash/sc-ci-base:focal_rust-nightly_latest
+zencash/sc-ci-base:focal_rust-stable_jdk-11_latest
+zencash/sc-ci-base:focal_rust-stable_jdk-16_latest
+zencash/sc-ci-base:focal_rust-stable_jdk-8_latest
+zencash/sc-ci-base:focal_rust-stable_latest
+zencash/sc-ci-base:hirsute_jdk-11_latest
+zencash/sc-ci-base:hirsute_jdk-16_latest
+zencash/sc-ci-base:hirsute_jdk-8_latest
+zencash/sc-ci-base:hirsute_rust-nightly_jdk-11_latest
+zencash/sc-ci-base:hirsute_rust-nightly_jdk-16_latest
+zencash/sc-ci-base:hirsute_rust-nightly_jdk-8_latest
+zencash/sc-ci-base:hirsute_rust-nightly_latest
+zencash/sc-ci-base:hirsute_rust-stable_jdk-11_latest
+zencash/sc-ci-base:hirsute_rust-stable_jdk-16_latest
+zencash/sc-ci-base:hirsute_rust-stable_jdk-8_latest
+zencash/sc-ci-base:hirsute_rust-stable_latest
+```
---
+++ ci/build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+
+set -euo pipefail
+
+get_work () {
+  # create array of images and build args, each element of format 'build_arg1,build_arg2,...;image_tag;dockerfile'
+  local build_args=""
+  local image_tag=""
+  local dockerfile=""
+  # add single flavor images to BUILD
+  for flavor in $(tr "," " " <<< "$FLAVORS"); do
+    build_args=""
+    image_tag=""
+    dockerfile=""
+    build_args+="FROM_IMAGE=${BASE_IMAGE},"
+    build_args+="ARG_GOSU_VERSION=${GOSU_VERSION},"
+    if grep -q rust <<< "$flavor"; then
+      build_args+="ARG_RUST_TOOLCHAIN=$(cut -d- -f2 <<< "$flavor"),"
+      build_args+="ARG_RUST_CROSS_TARGETS=${RUST_CROSS_TARGETS};"
+      image_tag="${CODENAME}_${flavor}_latest;"
+      dockerfile="./dockerfiles/${DISTRIBUTION}/Dockerfile_rust"
+    fi
+    if grep -q jdk <<< "$flavor"; then
+      build_args+="ARG_JDK_VERSION=$(cut -d- -f2 <<< "$flavor"),"
+      build_args+="ARG_JDK_JVM=${JDK_JVM},"
+      build_args+="ARG_MVN_VERSION=${MVN_VERSION};"
+      image_tag="${CODENAME}_${flavor}_latest;"
+      dockerfile="./dockerfiles/${DISTRIBUTION}/Dockerfile_jdk"
+    fi
+    BUILD+=( "${build_args}${image_tag}${dockerfile}" )
+  done
+  # add combined flavor images to BUILD
+  if [ -n "${COMBINE:-}" ]; then
+    local first_img=()
+    local second_img=()
+    local element=""
+    local first=""
+    local second=""
+    # array of rust-stable/nightly
+    mapfile -t first_img < <(tr " " "\n" <<< "${BUILD[@]}" | grep "$(cut -d, -f1 <<< "$COMBINE")" | cut -d";" -f2 | cut -d_ -f2)
+    # array of jdk-x
+    mapfile -t second_img < <(tr " " "\n" <<< "${BUILD[@]}" | grep "$(cut -d, -f2 <<< "$COMBINE")" | cut -d";" -f2 | cut -d_ -f2)
+    for element in "${BUILD[@]}"; do
+      for second in "${second_img[@]}"; do
+        if grep -q "$second" <<< "$element"; then
+          for first in "${first_img[@]}"; do
+            local append=""
+            # replace flavor with ${first}_${second}
+            append="${element//${second}/${first}_${second}}"
+            # replace BASE_IMAGE with ${DOCKER_ORG}\/${IMAGE_NAME}:${CODENAME}_${first}_latest
+            append="${append//${BASE_IMAGE}/${DOCKER_ORG}\/${IMAGE_NAME}:${CODENAME}_${first}_latest}"
+            BUILD+=( "$append" )
+          done
+        fi
+      done
+    done
+  fi
+  # for each element in BUILD add to TAG where we replace '_latest' with '_$DATE'
+  local element=""
+  echo "Images to build:"
+  for element in "${BUILD[@]}"; do
+    TAGS+=( "${element//_latest/_$DATE}" )
+    echo "${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "$element")"
+  done
+  echo
+  echo "Images to tag:"
+  local i=0
+  for i in "${!BUILD[@]}"; do
+    echo "${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${BUILD[$i]}") AS ${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${TAGS[$i]}")"
+  done
+}
+
+build_images () {
+  local image=""
+  local args=""
+  local arg=""
+  local tag=""
+  local dockerfile=""
+  for image in "${BUILD[@]}"; do
+    args=""
+    for arg in $(cut -d";" -f1 <<< "$image" | tr "," " "); do
+      args+=" --build-arg $arg"
+    done
+    tag="${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "$image")"
+    dockerfile="$(cut -d";" -f3 <<< "$image")"
+    echo "Running docker build $args -t $tag -f $dockerfile"
+    # shellcheck  disable=SC2086
+    docker build $args -t "$tag" -f "$dockerfile" .
+  done
+}
+
+tag_images () {
+  local i=0
+  for i in "${!BUILD[@]}"; do
+    echo "Tagging ${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${BUILD[$i]}") AS ${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${TAGS[$i]}")"
+    docker tag "${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${BUILD[$i]}")" "${DOCKER_ORG}/${IMAGE_NAME}:$(cut -d";" -f2 <<< "${TAGS[$i]}")"
+  done
+}
+
+push_images () {
+  local image=""
+  for image in $(docker image ls --filter=reference="${DOCKER_ORG}/${IMAGE_NAME}" --format "{{.Repository}}:{{.Tag}}"); do
+    echo "Pushing $image to registry"
+    docker push "$image"
+  done
+}
+
+DATE="$(date +%Y%m%d)"
+DISTRIBUTION="$(cut -d: -f1 <<< "$BASE_IMAGE")"
+CODENAME="$(cut -d: -f2 <<< "$BASE_IMAGE")"
+BUILD=()
+TAGS=()
+get_work
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+build_images
+tag_images
+if [ "${TRAVIS_BRANCH:-x}" = "master" ]; then
+  push_images
+fi
